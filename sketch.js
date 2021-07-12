ac33822682c29b5d64b5259b94cafe8cf3afbe61
Aarav <aarav.anand29@gmail.com>
2021-07-12 21:40:51 +0300

    constraint class

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log.body);
 
 }
 
@@ -60,5 +64,7 @@ function draw(){
     log5.display();
 
     bird.display();
+    log.display();
+    chain.display();
     platform.display();
 }
